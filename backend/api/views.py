a96c6374910efffd7309f4dddec7876a9b3c7d07
akumar23 <kumar.aryan@gmail.com>
2022-08-09 12:40:53 -0700

    added model to API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,18 @@
+from itertools import product
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
-
+from products.models import Product
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    body = request.body
-    data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
     
+    model_data = Product.objects.all().order_by("?").first()
+
+    data = {}
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,8 @@
 import requests
 
+#set endpoint to localhost server
 endpoint = "http://127.0.0.1:8000/api/"
 
-response = requests.get(endpoint)
+#get response with abc:123 as a param and hellow as a json query
+response = requests.get(endpoint, json={"product_id": 123})
 print(response.json())
