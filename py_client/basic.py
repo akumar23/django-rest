a0311f13d54995c633dd7fc96273a179e89c4abc
akumar23 <kumar.aryan@gmail.com>
2022-08-09 11:24:03 -0700

    more api updates

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,6 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/"
 
 response = requests.get(endpoint)
-print(response.text)
+print(response.json())
