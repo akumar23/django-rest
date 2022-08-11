8e6a7af24cb1bc3532b08ff61aebaab76d7708ee
akumar23 <kumar.aryan@gmail.com>
2022-08-10 19:32:30 -0700

    added rest framework calls

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from products.models import Product
+from django.forms.models import model_to_dict
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
     
     model_data = Product.objects.all().order_by("?").first()
 
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
 
     return JsonResponse(data)
