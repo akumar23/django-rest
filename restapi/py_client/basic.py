4bf3481d64a2427c9d4b35860aa53a7c806497e4
Aryan Kumar <kumar.aryan@gmail.com>
2022-08-13 16:28:28 -0700

    Delete restapi directory

--- restapi/py_client/basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-#set endpoint to localhost server
-endpoint = "http://127.0.0.1:8000/api/"
-
-#get response with abc:123 as a param and hellow as a json query
-response = requests.get(endpoint, json={"product_id": 123})
-print(response.json())
