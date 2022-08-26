68f04b8294f7ab693cdd81dc19b9b866bf27f500
Aryan Kumar <kumar.aryan@gmail.com>
2022-08-26 13:56:42 -0700

    started list function to enable the API to list every product

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,29 @@
+from itertools import product
 from django.shortcuts import render
+from rest_framework import generics
 
 # Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+        serializer.save()
+
+product_create_view = ProductCreateAPIView.as_view()
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_list_view = ProductListAPIView.as_view()
---
+++ py_client/create.py
@@ -0,0 +1,12 @@
+import requests
+
+#set endpoint to localhost server
+endpoint = "http://127.0.0.1:8000/api/products/"
+
+data = {
+    "title":"data1"
+}
+
+#get response with abc:123 as a param and hellow as a json query
+response = requests.post(endpoint, json=data)
+print(response.json())
