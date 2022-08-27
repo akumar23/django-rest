0784b8901a81c07e4e3fa3047a719b76067fc91d
Aryan Kumar <kumar.aryan@gmail.com>
2022-08-26 23:35:00 -0700

    added update functionality to API

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,9 @@
 from itertools import product
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 from .models import Product
@@ -26,8 +29,43 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 
 product_detail_view = ProductDetailAPIView.as_view()
 
-class ProductListAPIView(generics.ListAPIView):
+class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    lookup_field = 'pk'
 
-product_list_view = ProductListAPIView.as_view()
+    def perform_update(self, serializer):
+        instanse = serializer.save()
+        if not instanse.content:
+            instanse.content = instanse.title
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        if pk is not None:
+            #detail view
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response()
+        else:
+            #list view
+            queryset = Product.objects.all()
+            data = ProductSerializer(queryset, many=True).data
+            return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None 
+            if content is None:
+                content = title     
+            serializer.save(content=content)
+            return Response(serializer.data)
+        
+        return Response({"invalid": "invalid data inputted"}, status=400)
+
