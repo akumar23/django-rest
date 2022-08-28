e9176903eed0f174ac5c746cd62ac4f8c8612fd2
Aryan Kumar <kumar.aryan@gmail.com>
2022-08-27 20:45:07 -0700

    added delete/destroy functionality

--- backend/products/views.py
+++ backend/products/views.py
@@ -20,13 +20,11 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
             content = title
         
         serializer.save(content=content)
-
 product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
 product_detail_view = ProductDetailAPIView.as_view()
 
 class ProductUpdateAPIView(generics.UpdateAPIView):
@@ -38,8 +36,16 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
         instanse = serializer.save()
         if not instanse.content:
             instanse.content = instanse.title
-
 product_update_view = ProductUpdateAPIView.as_view()
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+product_destroy_view = ProductDestroyAPIView.as_view()
 
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
