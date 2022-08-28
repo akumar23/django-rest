26e0e356d8cc55d145d65df5d459bd399bc0d6d8
Aryan Kumar <kumar.aryan@gmail.com>
2022-08-27 21:31:17 -0700

    started creating mixin views

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,6 @@
 from itertools import product
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 from .models import Product
 from .serializers import ProductSerializer
+
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -22,10 +23,12 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         serializer.save(content=content)
 product_list_create_view = ProductListCreateAPIView.as_view()
 
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 product_detail_view = ProductDetailAPIView.as_view()
+
 
 class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
@@ -38,6 +41,7 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
             instanse.content = instanse.title
 product_update_view = ProductUpdateAPIView.as_view()
 
+
 class ProductDestroyAPIView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -46,6 +50,17 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
     def perform_destroy(self, instance):
         super().perform_destroy(instance)
 product_destroy_view = ProductDestroyAPIView.as_view()
+
+
+class ProductMixinView(mixins.ListModelMixin, generics.GenericAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+product_mixin_view = ProductMixinView.as_view()
 
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
